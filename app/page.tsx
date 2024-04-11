d508104a86f1c5b9e14dc9fddf7e6b4951744f09
Karen Lui <klui.work2@gmail.com>
2024-04-11 19:14:02 +0800

    landing page changes2

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,11 @@
-import { Poppins } from "next/font/google";
-
 import { Button } from "@/components/ui/button";
 import { IoFemale, IoMale } from "react-icons/io5";
-import { MdSick } from "react-icons/md";
 import Link from "next/link";
 import MaxWidthWrapper from "@/components/maxwidthwrapper";
-import { Faq } from "@/components/faq";
 
 import Image from "next/image";
 import { dietsvg, endocrinesvg, fatsvg, sportssvg } from "@/constants";
-
-const font = Poppins({
-  subsets: ["latin"],
-  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
-});
+import { FaArrowAltCircleDown } from "react-icons/fa";
 
 export default function Home() {
   return (
@@ -57,84 +49,102 @@ export default function Home() {
               <img src="/oldman.png" alt="" />
             </div>
           </div>
+          <div className="flex flex-col items-center pt-5">
+            <h2 className="text-3xl font-bold tracking-tight text-primary sm:text-4xl">
+              了解更多
+            </h2>
+            <FaArrowAltCircleDown
+              className="animate-bounce w-8 h-8 text-primary"
+              href="#section02"
+            />
+
+            <Link href={"#section02"}></Link>
+          </div>
         </MaxWidthWrapper>
       </section>
-      <section className="bg-gradient-to-tr from-purple-400 to-green-700">
-        <img
-          src="/why_1920x1280.jpg"
-          className="w-full h-full object-cover absolute mix-blend-overlay"
-        />
-        <div className="container flex flex-col mx-auto lg:flex-row">
-          <div className="flex flex-col w-full py-6 lg:w-2/3 md:py-8 lg:py-12">
-            <h2 className="text-3xl font-semibold leading-none ">
-              代謝症候群的成因
-            </h2>
-            <p className="pt-5 dark:text-gray-600">導致因素包括：</p>
-            <div className="container mx-auto grid justify-center gap-4 sm:grid-cols-2 lg:grid-cols-2">
-              <div className="flex flex-col items-center p-4">
-                <svg
-                  viewBox="0 0 160 160"
-                  fill="currentColor"
-                  className="w-32 h-32 text-blue-600"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d={endocrinesvg}
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-                <h3 className="my-3 text-2xl font-semibold">內分泌失調</h3>
-              </div>
-              <div className="flex flex-col items-center p-4">
-                <svg
-                  viewBox="0 0 160 160"
-                  fill="currentColor"
-                  className="w-32 h-32 text-blue-600"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d={dietsvg}
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-                <h3 className="my-3 text-2xl font-semibold">
-                  不適當的飲食習慣
-                </h3>
-              </div>
-              <div className="flex flex-col items-center p-4">
-                <svg
-                  viewBox="0 0 160 160"
-                  fill="currentColor"
-                  className="w-32 h-32 text-blue-600"
-                >
-                  <path fillRule="evenodd" d={fatsvg} clipRule="evenodd"></path>
-                </svg>
-                <h3 className="my-3 text-2xl font-semibold">肥胖</h3>
-              </div>
-              <div className="flex flex-col items-center p-4">
-                <svg
-                  viewBox="0 0 160 160"
-                  fill="currentColor"
-                  className="w-32 h-32 text-blue-600"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d={sportssvg}
-                    clipRule="evenodd"
-                  ></path>
-                </svg>
-                <h3 className="my-3 text-2xl font-semibold">缺乏運動</h3>
+      <section id="section02">
+        <div className="min-h-screen flex items-center justify-center px-16">
+          <div className="relative w-full max-w-7xl">
+            <div className="absolute top-0 -left-4 w-72 h-72 bg-purple-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob"></div>
+            <div className="absolute top-0 -right-4 w-96 h-96 bg-blue-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-2000"></div>
+            <div className="absolute -bottom-8 left-20 w-72 h-72 bg-pink-300 rounded-full mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000"></div>
+            <div className="m-8 relative space-y-4">
+              <div className="py-20 px-5 bg-white rounded-lg flex flex-col items-center justify-center space-x-8">
+                <h1 className="text-6xl font-semibold leading-none ">
+                  代謝症候群的成因
+                </h1>
+                <p className="pt-5 dark:text-gray-600">導致因素包括：</p>
+                <div className="container mx-auto grid justify-center gap-4 sm:grid-cols-2 lg:grid-cols-2">
+                  <div className="flex flex-col items-center p-4">
+                    <svg
+                      viewBox="0 0 160 160"
+                      fill="currentColor"
+                      className="w-32 h-32 text-blue-600"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d={endocrinesvg}
+                        clipRule="evenodd"
+                      ></path>
+                    </svg>
+                    <h3 className="my-3 text-2xl font-semibold">內分泌失調</h3>
+                  </div>
+                  <div className="flex flex-col items-center p-4">
+                    <svg
+                      viewBox="0 0 160 160"
+                      fill="currentColor"
+                      className="w-32 h-32 text-blue-600"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d={dietsvg}
+                        clipRule="evenodd"
+                      ></path>
+                    </svg>
+                    <h3 className="my-3 text-2xl font-semibold">
+                      不適當的飲食習慣
+                    </h3>
+                  </div>
+                  <div className="flex flex-col items-center p-4">
+                    <svg
+                      viewBox="0 0 160 160"
+                      fill="currentColor"
+                      className="w-32 h-32 text-blue-600"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d={fatsvg}
+                        clipRule="evenodd"
+                      ></path>
+                    </svg>
+                    <h3 className="my-3 text-2xl font-semibold">肥胖</h3>
+                  </div>
+                  <div className="flex flex-col items-center p-4">
+                    <svg
+                      viewBox="0 0 160 160"
+                      fill="currentColor"
+                      className="w-32 h-32 text-blue-600"
+                    >
+                      <path
+                        fillRule="evenodd"
+                        d={sportssvg}
+                        clipRule="evenodd"
+                      ></path>
+                    </svg>
+                    <h3 className="my-3 text-2xl font-semibold">缺乏運動</h3>
+                  </div>
+                </div>
+                <p className="mt-4 mb-8 p-regular-20 font-medium leading-6">
+                  內分泌失調、肥胖、不適當的飲食習慣和缺乏運動等多方面因素皆會導致代謝症候群。缺乏體育活動和不良飲食都與肥胖、高血壓、三酸甘油酯和高膽固醇有關。另外，其他疾病也可能增加代謝症候群的風險。例如，多囊卵巢綜合症、睡眠窒息症和抑鬱症等疾病都與代謝症候群有關。
+                  <br />
+                  <br />
+                  代謝症候群的病理學是與胰島素阻抗有關，即身體細胞對胰島素的反應不正常，無法有效利用葡萄糖作為能量。這導致血液中葡萄糖水平增加，進而引起胰臟產生更多的胰島素，進一步加劇胰島素阻抗和葡萄糖水平升高。此外，高脂肪飲食也會產生肝臟過氧化壓力、導致發炎和過氧化損傷。
+                  <br />
+                  <br />
+                  香港中文大學內科及藥物治療學系進行的研究顯示，隨著年齡的增長，代謝症候群的風險增加，老年人更容易患此疾病。
+                </p>
               </div>
             </div>
-            <p className="mt-4 mb-8 p-regular-20 font-medium leading-6">
-              內分泌失調、肥胖、不適當的飲食習慣和缺乏運動等多方面因素皆會導致代謝症候群。缺乏體育活動和不良飲食都與肥胖、高血壓、三酸甘油酯和高膽固醇有關。另外，其他疾病也可能增加代謝症候群的風險。例如，多囊卵巢綜合症、睡眠窒息症和抑鬱症等疾病都與代謝症候群有關。
-              <br />
-              <br />
-              代謝症候群的病理學是與胰島素阻抗有關，即身體細胞對胰島素的反應不正常，無法有效利用葡萄糖作為能量。這導致血液中葡萄糖水平增加，進而引起胰臟產生更多的胰島素，進一步加劇胰島素阻抗和葡萄糖水平升高。此外，高脂肪飲食也會產生肝臟過氧化壓力、導致發炎和過氧化損傷。
-              <br />
-              <br />
-              香港中文大學內科及藥物治療學系進行的研究顯示，隨著年齡的增長，代謝症候群的風險增加，老年人更容易患此疾病。
-            </p>
           </div>
         </div>
       </section>
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -18,6 +18,13 @@ const config = {
       },
     },
     extend: {
+      maxWidth: {
+        50: "50%",
+        60: "60%",
+        70: "70%",
+        80: "80%",
+        90: "90%",
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
@@ -70,10 +77,25 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        blob: {
+          "0%": {
+            transform: "translate(0px, 0px) scale(1)",
+          },
+          "33%": {
+            transform: "translate(30px, -50px) scale(1.1)",
+          },
+          "66%": {
+            transform: "translate(-20px, 20px) scale(0.9)",
+          },
+          "100%": {
+            transform: "tranlate(0px, 0px) scale(1)",
+          },
+        },
       },
       animation: {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
+        blob: "blob 7s infinite",
       },
     },
   },
