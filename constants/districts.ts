58fddf2e13d13ddea27c66c81b97e4069941d621
louise-longsze <louise-longsze>
2024-09-07 00:48:10 +0800

    feat: geolocation

--- constants/districts.ts
+++ constants/districts.ts
@@ -1,21 +1,78 @@
-export const DISTRICTS = [
-  "九龍城",
-  "大埔",
-  "中西區",
-  "元朗",
-  "屯門",
-  "北區",
-  "西貢",
-  "沙田",
-  "東區",
-  "油尖旺",
-  "南區",
-  "粉嶺",
-  "荃灣",
-  "深水埗",
-  "黃大仙",
-  "葵青",
-  "離島",
-  "灣仔",
-  "觀塘",
-];
+export const DISTRICTS = {
+  九龍城: {
+    latitude: 22.3363,
+    longitude: 114.1939,
+  },
+  大埔: {
+    latitude: 22.4444,
+    longitude: 114.1648,
+  },
+  中西區: {
+    latitude: 22.287,
+    longitude: 114.149,
+  },
+  元朗: {
+    latitude: 22.4451,
+    longitude: 114.0345,
+  },
+  屯門: {
+    latitude: 22.3925,
+    longitude: 113.9732,
+  },
+  北區: {
+    latitude: 22.4914,
+    longitude: 114.1879,
+  },
+  西貢: {
+    latitude: 22.2665,
+    longitude: 114.2601,
+  },
+  沙田: {
+    latitude: 22.3796,
+    longitude: 114.2029,
+  },
+  東區: {
+    latitude: 22.283,
+    longitude: 114.2244,
+  },
+  油尖旺: {
+    latitude: 22.3088,
+    longitude: 114.1692,
+  },
+  南區: {
+    latitude: 22.2325,
+    longitude: 114.181,
+  },
+  粉嶺: {
+    latitude: 22.4965,
+    longitude: 114.1345,
+  },
+  荃灣: {
+    latitude: 22.3668,
+    longitude: 114.1052,
+  },
+  深水埗: {
+    latitude: 22.3336,
+    longitude: 114.1645,
+  },
+  黃大仙: {
+    latitude: 22.3468,
+    longitude: 114.1962,
+  },
+  葵青: {
+    latitude: 22.366,
+    longitude: 114.1339,
+  },
+  離島: {
+    latitude: 22.197,
+    longitude: 113.942,
+  },
+  灣仔: {
+    latitude: 22.2839,
+    longitude: 114.1757,
+  },
+  觀塘: {
+    latitude: 22.31,
+    longitude: 114.2245,
+  },
+};
